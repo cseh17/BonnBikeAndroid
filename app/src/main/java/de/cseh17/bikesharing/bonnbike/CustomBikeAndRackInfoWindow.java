fec3c4a91dd20e23bc985a5a2c72c14bc11b00e9
cseh17 <krisztian.cseh@smail.th-koeln.de>
2019-07-07 15:02:39 +0200

    Implementation: - display available bike number for racks
    Layout:
    - display available bikes for rack in green color
    - display bike number in bold big numbers

--- app/src/main/java/de/cseh17/bikesharing/bonnbike/CustomBikeAndRackInfoWindow.java
+++ app/src/main/java/de/cseh17/bikesharing/bonnbike/CustomBikeAndRackInfoWindow.java
@@ -1,6 +1,7 @@
 package de.cseh17.bikesharing.bonnbike;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.TextView;
@@ -19,9 +20,11 @@ public class CustomBikeAndRackInfoWindow implements GoogleMap.InfoWindowAdapter 
 
     private final View customBikeAndRackInfoWindowView;
     private static Place clickedClusterItem;
+    private Context receivedContext;
 
     CustomBikeAndRackInfoWindow(Context mContext) {
         customBikeAndRackInfoWindowView = LayoutInflater.from(mContext).inflate(R.layout.custom_bike_and_rack_info_window, null);
+        receivedContext = mContext;
     }
 
     // Method that gets the selected marker in order to be able to display the current marker information.
@@ -32,10 +35,28 @@ public class CustomBikeAndRackInfoWindow implements GoogleMap.InfoWindowAdapter 
     @Override
     public View getInfoWindow(Marker marker) {
 
-        // Set InfoWindow title
-        TextView ciwTitleTv = customBikeAndRackInfoWindowView.findViewById(R.id.ciw_title);
-        ciwTitleTv.setText( clickedClusterItem.getName());
-        customBikeAndRackInfoWindowView.setClickable(false);
+        // Check if the marker is a bike or a rack, and configure it accordingly
+        if (clickedClusterItem.getBike()) {
+            TextView ciwTitleTv = customBikeAndRackInfoWindowView.findViewById(R.id.ciw_title);
+            ciwTitleTv.setText(receivedContext.getString(R.string.custom_info_window_bike_name_title));
+            TextView ciwSubtitleTv = customBikeAndRackInfoWindowView.findViewById(R.id.ciw_subtitle);
+            ciwSubtitleTv.setText(clickedClusterItem.getBikeNumbers().get(0));
+            ciwSubtitleTv.setTextColor(Color.BLACK);
+            ciwSubtitleTv.setTextSize(35);
+        } else {
+
+            // Set InfoWindow title
+            TextView ciwTitleTv = customBikeAndRackInfoWindowView.findViewById(R.id.ciw_title);
+            ciwTitleTv.setText(clickedClusterItem.getName());
+
+            // Set rack information like free racks and available bikes
+            TextView ciwSubtitleTv = customBikeAndRackInfoWindowView.findViewById(R.id.ciw_subtitle);
+            ciwSubtitleTv.setText(receivedContext.getString(R.string.custom_info_window_rack_available_bike_title) + clickedClusterItem.getBikes());
+            ciwSubtitleTv.setTextColor(receivedContext.getColor(R.color.darkGreen));
+            ciwSubtitleTv.setTextSize(18);
+        }
+
+
         return customBikeAndRackInfoWindowView;
     }
 
