c73f333085c4119211d31566023a1adda784a8d2
cseh17 <krisztian.cseh@smail.th-koeln.de>
2019-07-30 21:21:22 +0200

    UI: - created splashScreen Develope: - added SplashActivity to handle splashScreen

---
+++ app/src/main/java/de/cseh17/bikesharing/bonnbike/SplashActivity.java
@@ -0,0 +1,22 @@
+package de.cseh17.bikesharing.bonnbike;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (!isTaskRoot() && getIntent().hasCategory(Intent.CATEGORY_LAUNCHER) && getIntent().getAction() != null && getIntent().getAction().equals(Intent.ACTION_MAIN)) {
+            finish();
+            return;
+        }
+
+        startActivity(new Intent(SplashActivity.this, MapsActivity.class));
+        finish();
+    }
+}
